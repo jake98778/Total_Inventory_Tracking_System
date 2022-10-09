7945e0036b76f54fefc9ee7f5c7468420ecc3bd0
jake98778 <jake98778@gmail.com>
2022-10-09 15:35:56 -0400

    Login pages example. Skipping this and making actual flow after login setup.
    Currently got login screens setup but I don't like this at all. Lets skip authentication and get a product made. We can worry about login and shit later. Lets get the idea working without anything but a app and database entry. Skipping the stuff we need to come back for.

--- TITS.py
+++ TITS.py
@@ -1,32 +1,98 @@
 from kivy.app import App
 from kivy.lang import Builder
-import requests
-#tested requests already and this is working currently. Must be prebuilt into python so opop
+from kivy.uix.screenmanager import ScreenManager, Screen
 
-#below is copied code from a open source repo. This is zbarcam example proj currently running
-#we need to make this a selectable section in the app. So need to build a paging system first
-#App flow diagram is in the wiki.
+# Create both screens. Please note the root.manager.current: this is how
+# you can control the ScreenManager from kv. Each screen has by default a
+# property manager that gives you the instance of the ScreenManager used.
+Builder.load_string("""
+<MenuScreen>:
+    GridLayout:
 
-DEMO_APP_KV_LANG = """
-#:import ZBarCam kivy_garden.zbarcam.ZBarCam
-BoxLayout:
-    orientation: 'vertical'
-    ZBarCam:
-        id: zbarcam
-        # optional, by default checks all types
-        code_types: 'QRCODE', 'EAN13'
-    Label:
-        size_hint: None, None
-        size: self.texture_size[0], 50
-        text: ', '.join([str(symbol.data) for symbol in zbarcam.symbols])
-"""
+        cols: 2
+        rows: 2
+        row_force_default: True
+        row_default_height: 40
 
+        Button:
+            text: 'Login'
+            on_press: root.manager.current = 'login'
+            size_hint_x: None
+            width: 100
+        Button:
+            text: 'Sign Up'
+            on_press: root.manager.current = 'signup'
+            size_hint_x: None
+            width: 100
 
-class DemoApp(App):
+<LoginScreen>:
+    GridLayout:
+
+        cols: 2
+        rows: 2
+        row_force_default: True
+        row_default_height: 40
+
+        Button:
+            text: 'login page'
+            size_hint_x: None
+            width: 100
+        Button:
+            text: 'Menu'
+            on_press: root.manager.current = 'menu'
+            size_hint_x: None
+            width: 100
+        TextInput
+            text: 'email'
+            size_hint_x: None
+            width: 100
+            hight: 50
+        TextInput
+            text: 'Password'
+            size_hint_x: None
+            width: 100
+            hight: 50
+
+<SignupScreen>:
+    GridLayout:
+
+        cols: 2
+        rows: 2
+        row_force_default: True
+        row_default_height: 40
+
+        Button:
+            text: 'SignUp page'
+            size_hint_x: None
+            width: 100
+        Button:
+            text: 'Menu'
+            on_press: root.manager.current = 'menu'
+            size_hint_x: None
+            width: 100
+
+""")
+
+# Declare both screens
+class MenuScreen(Screen):
+    pass
+
+class LoginScreen(Screen):
+    pass
+
+class SignupScreen(Screen):
+    pass
+
+class TestApp(App):
 
     def build(self):
-        return Builder.load_string(DEMO_APP_KV_LANG)
+        # Create the screen manager
+        sm = ScreenManager()
+        sm.add_widget(MenuScreen(name='menu'))
+        sm.add_widget(LoginScreen(name='login'))
+        sm.add_widget(SignupScreen(name='signup'))
 
+        return sm
 
 if __name__ == '__main__':
-    DemoApp().run()
+    TestApp().run()
